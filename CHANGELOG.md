aa4b222a9256e9dfd1ee318297e46517ab8c1f83
邱文也 <wayne900204@gmail.com>
2021-08-29 08:13:49 +0800

    [Feature]新增Api Route

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,10 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
+## [0.0.6] - 2021-08-29
+### Added
+- 新增 API Route
+
 ## [0.0.5] - 2021-08-28
 - 新增 relationShip 的 Seeder
 - 產生隨機亂數 id，並放在 Helpers 裡面
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/users',[UserController::class,'index']);
+Route::get('/users/{user}', [UserController::class,'show']);
+Route::post('/users', [UserController::class,'store']);
+Route::patch('/users/{user}', [UserController::class,'update']);
+Route::delete('/users/{user}', [UserController::class,'destroy']);
+
+//Route::apiResource('users',UserController::class);
